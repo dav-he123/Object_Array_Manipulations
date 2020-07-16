4b2fdb24a97545a885180a57a72706a4481ebb9b
David <david.he07@yahoo.ca>
2020-07-16 17:07:30 -0400

    updated code for question 5

--- object-array-manipulations.js
+++ object-array-manipulations.js
@@ -119,8 +119,98 @@ const metalsByType = function (metals) {
     result[arrElement["type"]].push(arrElement["name"]);
   }
 
-  console.log(result);
+  // console.log(result);
   return result;
 };
 
 metalsByType(metals);
+
+// Question 4: Create a list of all metals sorted by their carbon concentration (in descending order)
+
+// ["1084", "80CrV2","1075", "S30V", "420HC"]
+
+let resultMetals = metals.sort(function (x, y) {
+  return y.composition.carbon - x.composition.carbon;
+});
+
+// let resultMetals = metals.sort((a, b) =>
+//   a.composition.carbon < b.composition.carbon
+//     ? 1
+//     : a.composition.carbon > b.composition.carbon
+//     ? -1
+//     : 0
+// );
+
+// console.log(resultMetals);
+
+const metalsList = function (resultMetals) {
+  let result = [];
+
+  for (const element of resultMetals) {
+    result.push(element["name"]);
+  }
+
+  // console.log(result);
+
+  return result;
+};
+
+metalsList(metals);
+
+// ["1084", "80CrV2","1075", "S30V", "420HC"]
+
+// Question 5: Create a list of every element used in the composition of the metals, and store their average concentration
+
+let avgConcentration = function (metals) {
+  let result = {};
+  let finalResult = {};
+
+  for (const element of metals) {
+    // console.log(element["composition"]);
+    for (const [key] of Object.entries(element["composition"])) {
+      result[key] = [];
+    }
+  }
+
+  // console.log(result[key]);
+
+  for (const element of metals) {
+    for (const [key, value] of Object.entries(element["composition"])) {
+      // console.log(value);
+
+      result[key].push(value);
+    }
+  }
+
+  // console.log(result);
+
+  let arr;
+
+  for (const elem of Object.values(result)) {
+    arr = elem.reduce((a, b) => a + b, 0) / elem.length;
+    console.log(arr);
+  }
+
+  // console.log(arr);
+};
+
+let average = function (elem) {
+  let arr = elem.reduce((a, b) => a + b, 0) / elem.length;
+  return arr;
+};
+
+avgConcentration(metals);
+
+// {
+//   carbon: 0.004,
+//   chrome: 0.4,
+//   chromium: 0.12,
+//   iron: 98,
+//   manganese:0.01,
+//   molybdenium: 0.2,
+//   nickel:0.5,
+//   phosphorus: 0.025,
+//   silicon:0.6,
+//   sulfur: 0.025,
+//   vanadium:0.4,
+// }

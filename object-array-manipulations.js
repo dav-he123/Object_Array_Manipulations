dd300c4f465c855987e9a7840550896e66065837
David <david.he07@yahoo.ca>
2020-07-20 17:47:56 -0400

    updated code for part 4

--- object-array-manipulations.js
+++ object-array-manipulations.js
@@ -333,7 +333,7 @@ let averageFunc = function (elem) {
 
 metalsListAverage(metals);
 
-// Part 4 - Solving the questions differently
+// Part 3 - Solving the questions differently
 
 // Question 1: Create a list of all metals in the collection
 
@@ -383,9 +383,169 @@ let listMetalsSortedBy = function (metals) {
 
   resultMetals.forEach((element) => result.push(element.name));
 
-  console.log(result);
+  // console.log(result);
 };
 
 listMetalsSortedBy(metals);
 
 // Question 5: Create a list of every element used in the composition of the metals, and store their average concentration
+
+let listMetalsAverage = function (metals) {
+  let result = {};
+
+  for (const elements of metals) {
+    for (const keys of Object.keys(elements["composition"])) {
+      result[keys] = [];
+    }
+  }
+
+  for (const elements of metals) {
+    for (const [keys, values] of Object.entries(elements["composition"])) {
+      result[keys].push(values);
+    }
+  }
+
+  for (const [keys, values] of Object.entries(result)) {
+    result[keys] = averageFuncSecond(values).toFixed(3);
+  }
+
+  // console.log(result);
+};
+
+let averageFuncSecond = function (elem) {
+  let arr = elem.reduce((a, b) => a + b) / elem.length;
+  return arr;
+};
+
+listMetalsAverage(metals);
+
+// Part 4 - Solving the questions differently
+
+const metalsUpdate = [
+  {
+    name: "1084",
+    type: "Carbon Steel",
+    composition: {
+      iron: 98.08,
+      carbon: 0.8,
+      manganese: 0.6,
+      phosphorus: 0.05,
+      sulfur: 0.04,
+    },
+    forging_min_temp: 900,
+    forging_max_temp: 1200,
+  },
+  {
+    name: "1075",
+    type: "Carbon Steel",
+    composition: {
+      iron: 98,
+      carbon: 0.7,
+      manganese: 0.4,
+      phosphorus: 0.05,
+      sulfur: 0.04,
+    },
+    forging_min_temp: 900,
+    forging_max_temp: 1200,
+  },
+  {
+    name: "80CrV2",
+    type: "Carbon Steel",
+    composition: {
+      iron: 98.08,
+      carbon: 0.75,
+      chrome: 0.4,
+      molybdenium: 0.1,
+      vanadium: 0.15,
+      manganese: 0.3,
+      phosphorus: 0.025,
+      sulfur: 0.025,
+    },
+    forging_min_temp: 850,
+    forging_max_temp: 1100,
+  },
+  {
+    name: "S30V",
+    type: "Stainless Steel",
+    composition: {
+      iron: 98,
+      carbon: 0.015,
+      chromium: 0.14,
+      molybdenium: 0.2,
+      vanadium: 0.4,
+    },
+    forging_min_temp: 975,
+    forging_max_temp: 1200,
+  },
+  {
+    name: "420HC",
+    type: "Stainless Steel",
+    composition: {
+      iron: 98,
+      carbon: 0.004,
+      nickel: 0.5,
+      silicon: 0.6,
+      chromium: 0.12,
+      manganese: 0.01,
+    },
+    forging_min_temp: 1050,
+    forging_max_temp: 1200,
+  },
+];
+
+// Question 1: Create a list of all metals in the collection
+
+let listMetalsSecond = function (metalsUpdate) {
+  // console.log(metals);
+  let resultAns = [];
+
+  for (const element in metalsUpdate) {
+    // console.log(element);
+    resultAns.push(metalsUpdate[element].name);
+  }
+  // console.log(resultAns);
+
+  return resultAns;
+};
+
+listMetalsSecond(metalsUpdate);
+
+// Question 2: Create a list of all metals with their type in the collection
+
+let listMetalsForIn = function (metalsUpdate) {
+  // console.log(metals);
+  let resultAns = [];
+
+  for (const element in metalsUpdate) {
+    // console.log(element);
+    resultAns.push(
+      metalsUpdate[element].name + " " + metalsUpdate[element].type
+    );
+  }
+  // console.log(resultAns);
+
+  return resultAns;
+};
+
+listMetalsForIn(metalsUpdate);
+
+// Question 3: Create an object containing the metals by type
+
+let listMetalsByTypeForIn = function (metalsUpdate) {
+  // console.log(metals);
+  let resultAns = {};
+
+  for (const element in metalsUpdate) {
+    // console.log(metalsUpdate[element].type);
+    resultAns[metalsUpdate[element].type] = [];
+  }
+
+  for (const elem in metalsUpdate) {
+    resultAns[metalsUpdate[elem].type].push(metalsUpdate[elem].name);
+  }
+  console.log(resultAns);
+
+  return resultAns;
+};
+
+listMetalsByTypeForIn(metalsUpdate);
